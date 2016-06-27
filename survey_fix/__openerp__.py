e6f63f9986b51d7d6c6e67a7c891d619ca8778c1
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-06-27 09:51:08 -0300

    add survey fix

---
+++ survey_fix/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    School
+#    Copyright (C) 2014 No author.
+#    No email
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'author': 'ADHOC SA',
+    'auto_install': False,
+    'installable': True,
+    'category': 'Tools',
+    'demo': [
+    ],
+    'depends': ['survey'],
+    'description': 'Survey Fix',
+    'init_xml': [],
+    'license': 'AGPL-3',
+    'name': u'Survey Fix',
+    'test': [],
+    'data': [
+    ],
+    'version': '8.0.0.0.0',
+    'website': ''}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
