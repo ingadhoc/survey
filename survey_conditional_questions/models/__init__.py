0635b3626e425136a2f660a44d582ca5a4d959f2
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-08-20 17:34:12 -0300

    [REM] survey_conditional_questions no longer needed and also with a bug. On odoo v13 this is already in odoo If needed, we should fix/improve "def validate_question(self, post, answer_tag):"

--- survey_conditional_questions/models/__init__.py
+++
@@ -1,6 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import survey_question
-from . import survey_user_input
